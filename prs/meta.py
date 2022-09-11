5e88f30e33cd4b128b4a14a3ac8c37652f7794db
DvGils <demian_vg@hotmail.nl>
2022-09-11 18:10:20 +0200

    documentation

--- prs/meta.py
+++ prs/meta.py
@@ -1,5 +1,11 @@
 """
 |  Wrapper for pyreadstat to easily read, create and adjust .sav files.
+|  
+|  **Installation:**
+|       ``pip install prs-meta``
+|
+|  **Usage:**
+|       ``from prs.meta import Meta``
 |
 |  **Requirements:**
 |       `pyreadstat 1.1.6 <https://ofajardo.github.io/pyreadstat_documentation/_build/html/index.html>`_
@@ -12,6 +18,16 @@ import pandas as pd
 class Meta:
     """
     Wrapper for pyreadstat to easily read, create and adjust .sav files.
+
+    :attributes:
+        ``df``: pandas DataFrame
+        ``meta``: original meta object from pyreadstat (if provided)
+        ``names``: list of column names
+        ``labels``: dict of column names mapped to column labels
+        ``value_labels``: dict of column names mapped to value labels
+        ``types``: dict of column names mapped to their type in SPSS format
+        ``measures``: dict of column names mapped to their measure (nominal, ordinal, scale)
+        ``missing``: dict of column names mapped to their missing ranges
 
     :param path: path to an SPSS file. The DataFrame and meta data object will be constructed from this file, if provided.
     :type path: str, optional
